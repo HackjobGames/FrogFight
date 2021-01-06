1421586661e8920d743621acdecf10112387d374
Grant-Helwig <grant.helwig@gmail.com>
2021-01-06 12:15:30 -0800

    Revert "added simple grapple system"
    This reverts commit 0fd24584a4e26d45dddc60849cd0768a0051c263.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,7 +7,6 @@ public class Character : MonoBehaviour
     public StateMachine movement_machine;
     public State running_state;
     public State jumping_state;
-    public State swinging_state;
     public State falling_state;
     [SerializeField]
     private float gravity = -9.81f;
@@ -22,24 +21,10 @@ public class Character : MonoBehaviour
     [SerializeField]
     private float stop_speed = 11f;
     [SerializeField]
-    private float max_tongue_distance = 100f;
     public float jump_force = 11f;
     public Collision collision;
-    [SerializeField]
-    private LineRenderer tongue;
-    private Vector3 grapple_point;
-     [SerializeField]
-    private LayerMask is_grappleable;
 
     private Rigidbody rigid_body;
-    private SpringJoint joint;
-
-      [SerializeField]
-    private Transform mouth;
-    [SerializeField]
-    private Transform main_camera;
-    [SerializeField]
-    private Transform player;
     public void Move(float speed_modifier, Vector3 direction){
         if(Mathf.Abs(rigid_body.velocity.magnitude) > max_speed) {
             rigid_body.velocity = Vector3.MoveTowards(rigid_body.velocity, direction * speed * speed_modifier, dec_speed * Time.deltaTime);
@@ -55,41 +40,8 @@ public class Character : MonoBehaviour
     public void ApplyImpulse(Vector3 force){
         rigid_body.AddForce(force, ForceMode.Impulse);
     }
-
-    public void StartGrapple(){
-        RaycastHit hit;
-
-        if(Physics.Raycast(main_camera.position, main_camera.forward, out hit, max_tongue_distance, is_grappleable)){
-            grapple_point = hit.point;
-            joint = player.gameObject.AddComponent<SpringJoint>();
-            joint.autoConfigureConnectedAnchor = false;
-            joint.connectedAnchor = grapple_point;
-
-            float dist_from_point = Vector3.Distance(player.position, grapple_point);
-            joint.maxDistance = dist_from_point * .8f;
-            joint.minDistance = dist_from_point * .3f;
-
-            joint.spring = 10f;
-            joint.damper = 7f;
-            joint.massScale = 4.5f;
-
-            tongue.positionCount = 2;
-        }
-
-    }
-
-    public void StopGrapple(){
-        tongue.positionCount = 0;
-        Destroy(joint);
-    }
-
-    public void UpdateTonguePositions(){
-        tongue.SetPosition(0,mouth.position);
-        tongue.SetPosition(1,grapple_point);
-    }
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
         rigid_body = GetComponent<Rigidbody>();
         collision = GetComponent<Collision>();
 
@@ -97,7 +49,6 @@ public class Character : MonoBehaviour
         running_state = new  RunningState(this,movement_machine);
         jumping_state = new JumpingState(this,movement_machine);
         falling_state = new FallingState(this,movement_machine);
-        swinging_state = new SwingingState(this,movement_machine);
 
         movement_machine.Initialize(running_state);
     }
