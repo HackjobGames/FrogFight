74a647b848b11fd89ad6775f1feeee5236d805e4
Dylan Kersting <kerst098@umn.edu>
2021-01-15 19:23:36 -0600

    implement network

---
+++ Assets/Scripts/network/NetworkTesting.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class NetworkTesting : MonoBehaviour
+{
+    public DarkReflectiveMirrorTransport transport;
+    public NetworkManager manager;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(transport.serverID);
+    }
+}
