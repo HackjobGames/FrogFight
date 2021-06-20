3588e40b7469182ad67b245f12171668c6bf1e80
Dylan Kersting <kerst098@umn.edu>
2021-06-20 18:11:51 -0500

    reset look at

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -311,6 +311,9 @@ public class Character : NetworkBehaviour
         spine.enabled = false;
         spine.transform.position = new Vector3(10000, 10000, 10000);      
         GetComponent<Character>().enabled = false;
+        if (this.isLocalPlayer) {
+          main_camera.GetComponent<CameraFollow>().lookAt = this.look_at;
+        }
     }
 }
 
